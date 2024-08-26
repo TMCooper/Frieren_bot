858021d317f16ca5b9169f56b2b18548f13fa73c
TMCooper# <pokemon160377@gmail.com>
2024-08-26 03:19:03 +0200

    Update python_bot.py

--- python_bot.py
+++ python_bot.py
@@ -1,1 +1,22 @@
+import discord
 import os
+import subprocess
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        subprocess.run('cls', shell=True)
+        print(f'{self.user} est reveiller !')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
