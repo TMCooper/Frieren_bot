c8ef9ed503b395f9ad427076373445976a644782
TMCooper# <pokemon160377@gmail.com>
2024-12-23 00:42:03 +0100

    Update Frieren.py

--- Frieren.py
+++ Frieren.py
@@ -1,10 +1,12 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import os
 import subprocess
+import asyncio
 from dotenv import load_dotenv
 from function.Maid import Maid
-import asyncio
+from function.Eru import Eru
 
 # Chargement des variables d'environnement
 load_dotenv()
@@ -65,24 +67,27 @@ async def hello_world(interaction: discord.Interaction):
 # Commande : /code
 @bot.tree.command(
     name="code",
-    description="Pour obtenir les code d'échange de Genshin ou hsr",
+    description="Pour obtenir les code d'échange de Genshin ou Honkai star rail",
 )
+@app_commands.describe(jeux_entrer="Le jeu pour lequel vous souhaitez récupérer les codes")
+@app_commands.choices(jeux_entrer=[
+    app_commands.Choice(name="Genshin", value="genshin"),
+    app_commands.Choice(name="Honkai Star Rail", value="hsr")
+])
 # @app_commands.describe(jeux="Le jeu pour lequel vous souhaitez récupérer les codes")
-async def code(interaction: discord.Interaction, jeux_entrer: str):
+async def code(interaction: discord.Interaction, jeux_entrer: app_commands.Choice[str]):
     # met les caractère en minuscule
-    jeu = jeux_entrer.lower()
+    jeu = jeux_entrer.value.lower()
 
     # envoie une reponse temporaire afin de montrer que le bot traite la commande
-    await interaction.response.send_message("Frieren réfléchie", ephemeral=True)
+    # await interaction.response.send_message("Frieren réfléchie", ephemeral=True)
+    await interaction.response.defer(ephemeral=True)
 
     # appelle la fonction Maid.scrap() pour récupérer les codes
     code_scrap = Maid.scrap(jeu)
 
-    # delay de 3 seconde
-    await asyncio.sleep(3)
-
     # envoie le code d'échange
-    await interaction.response.send_message(f"Code d'échange Genshin Impact : \n{code_scrap}")
+    await interaction.followup.send(f"Code d'échange : \n{code_scrap}")
 
 @bot.tree.command(
     name="my_id",
@@ -92,6 +97,39 @@ async def my_id(interaction: discord.Interaction):
     
     await interaction.response.defer(ephemeral=True)
     await interaction.followup.send(f"Votre ID : {interaction.user.id}")
+
+@bot.tree.command(
+    name="valorant_rank",
+    description="Donne le rank de l'utilisateur sur valorant",
+)
+@app_commands.describe(pseudo="Le pseudo de l'utilisateur")
+@app_commands.describe(tag="Le tag de l'utilisateur")
+async def valorant_rank(interaction: discord.Interaction, pseudo: str, tag: str):
+    await interaction.response.defer(ephemeral=True)
+    rank = Maid.valorant_tracker_rank(pseudo, tag)
+    await interaction.followup.send(f"Le rank de {pseudo}#{tag} est : {rank}")
+
+@bot.tree.command(
+    name="waifu",
+    description="Donne une waifu aléatoire",
+)
+async def waifu(interaction: discord.Interaction):
+    await interaction.response.defer()
+    name, alternate_name, age, birthday, height, weight, blood_type, waifu_classification, description, like_rank, popularity_rank, image_url = Eru.random_waifu()
+
+    embed = discord.Embed(title=name, description=description, color=discord.Color.blue())
+    embed.set_thumbnail(url=image_url)
+    embed.add_field(name="Alternate Name", value=alternate_name, inline=True)
+    embed.add_field(name="Age", value=age, inline=True)
+    embed.add_field(name="Birthday", value=birthday, inline=True)
+    embed.add_field(name="Height", value=height, inline=True)
+    embed.add_field(name="Weight", value=weight, inline=True)
+    embed.add_field(name="Blood Type", value=blood_type, inline=True)
+    embed.add_field(name="Waifu Classification", value=waifu_classification, inline=True)
+    embed.add_field(name="Like Rank", value=like_rank, inline=True)
+    embed.add_field(name="Popularity Rank", value=popularity_rank, inline=True)
+
+    await interaction.followup.send(embed=embed)
 
 # Démarrage du bot
 bot.run(TOKEN)
