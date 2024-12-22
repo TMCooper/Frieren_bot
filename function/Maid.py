ad6bd74cbb73146f762dface8aa3e0a4d5106aa3
TMCooper# <pokemon160377@gmail.com>
2024-12-23 00:41:48 +0100

    Update Maid.py

--- function/Maid.py
+++ function/Maid.py
@@ -2,24 +2,20 @@ from function.Yui import Yui
 
 class Maid:
     def scrap(jeu):
-        if jeu == "genshin":
-            return "genshin"
-    
-        elif jeu == "hsr":
-            links = Yui.requet_hsr()
-            codes = []
-            # Extraire le texte de chaque lien et chercher le code
-            for link in links:
-                # Vérifie si l'élément contient un attribut href
-                href = link.find('a', href=True)
-                if href and 'gift?code=' in href['href']:
-                    # Extraire le code depuis l'attribut href
-                    code = href['href'].split('gift?code=')[1].split('&')[0]
-                    codes.append(code)
-                elif 'gift?code=' in link.get_text():
-                    # Extraire le code depuis le texte brut
-                    code = link.get_text().split('gift?code=')[1].split('&')[0]
-                    codes.append(code)
+        links = Yui.requet_code(jeu)
+        codes = []
+        # Extraire le texte de chaque lien et chercher le code
+        for link in links:
+            # Vérifie si l'élément contient un attribut href
+            href = link.find('a', href=True)
+            if href and 'gift?code=' in href['href']:
+                # Extraire le code depuis l'attribut href
+                code = href['href'].split('gift?code=')[1].split('&')[0]
+                codes.append(code)
+            elif 'gift?code=' in link.get_text():
+                # Extraire le code depuis le texte brut
+                code = link.get_text().split('gift?code=')[1].split('&')[0]
+                codes.append(code)
 
         code_syntax = []
         code_syntax.append("Liste sous forme de tableau :")
@@ -36,3 +32,12 @@ class Maid:
 
         # Encapsuler dans des balises Markdown
         return code_discord_version
+    
+    def valorant_tracker_rank(pseudo, tag):
+        soup = Yui.valorant_request(pseudo, tag)
+        stat_value = soup.find('span', class_='stat__value')
+        if stat_value:
+            rank = stat_value.get_text(strip=True)
+            return rank
+        else:
+            return "Rank not found"
