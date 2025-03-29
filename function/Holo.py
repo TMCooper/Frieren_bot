01b74f8a4e447da5023ad5bc21845f653e5dd5c8
TMCooper <pokemon160377@gmail.com>
2025-03-29 15:37:19 +0100

    Update

--- function/Holo.py
+++ function/Holo.py
@@ -78,7 +78,7 @@ class Holo:
             if platform.system() == "Windows":
                 os.execv(sys.executable, ['python'] + sys.argv)
             elif platform.system() == "Linux":
-                subprocess.run('source ./venv/bin/activate && nohub python Frieren.py &', shell=True)
+                subprocess.run('source ./venv/bin/activate && sudo apt install -r requirements.txt && nohub python Frieren.py &', shell=True)
                 # os.execv(sys.executable, ['nohup python'] + sys.argv + [" &"])
         else:
             await interaction.followup.send("Vous n'êtes pas autorisé à mettre à jour le bot.")
