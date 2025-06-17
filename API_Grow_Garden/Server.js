e9c1acd669225b8ae32add351b39de9164f34cb4
TMCooper <pokemon160377@gmail.com>
2025-06-17 17:13:06 +0200

    Little update

--- API_Grow_Garden/Server.js
+++ API_Grow_Garden/Server.js
@@ -6,7 +6,7 @@ const blessed = require('blessed');
 const cors = require('cors');
 
 const configPath = path.join(__dirname, 'config.json');
-let config = { IPWhitelist: false, WhitelistedIPs: [], Dashboard: true, Port: 3000 };
+let config = { IPWhitelist: false, WhitelistedIPs: [], Dashboard: true, Port: 5521 };
 
 if (fs.existsSync(configPath)) {
   config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
